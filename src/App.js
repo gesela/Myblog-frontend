0c7ef6d0ab8bf13864aec5d81b75a547eea5fbd1
Tewodros Yimer <tyimers@gmail.com>
2022-07-06 00:42:44 -0400

    updating frontend

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Home from "./pages/homepage/Homepage";
+import TopBar from "./components/topbar/Topbar";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {  Routes, Route} from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
+
+
+function App() {
+  const { user ,setUser } = useContext(Context);
+  return (
+
+    <Routes>
+     <Route element={ <TopBar /> } />
+      {/* <Switch> */}
+        <Route exact path="/" element={<Home />}/>
+          
+        
+        <Route path="/register" element={user ? <Home /> : <Register />}/>
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />}/>
+        <Route path="/settings" element={user ? <Settings /> : <Register />}/>
+        <Route path="/post/:postId" element=
+          {<Single />} 
+        />
+      {/* </Switch> */}
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router} from "react-router-dom";
+import reportWebVitals from './reportWebVitals'
+import App from "./App";
+import { ContextProvider } from "./context/Context";
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <React.StrictMode>
+    <Router>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+    </Router>
+  </React.StrictMode>
+  
+);
+reportWebVitals()
+
